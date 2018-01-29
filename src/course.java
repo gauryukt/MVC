561c089efd4f3db092a376e53832777f3ea9044b
gauryukt <yuktigaur1325@gmail.com>
2018-01-28 22:37:50 -0500

    Update course.java

--- src/course.java
+++ src/course.java
@@ -1,1 +1,46 @@
+public class Course {
+	private String coursename;
+	private int coursenumber;
+  private int credit;
+  private String professor;
+	
+	public Course(String coursename, int coursenumber, int credit, String professor) {
+		this.coursename = coursename;
+		this.coursenumber = coursenumber;
+    this.credit = credit;
+    this.professor = professor;
 
+	}
+	
+	public String getcoursename() {
+		return this.coursename;
+	}
+	
+	public void setcoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	
+	
+	public int getcoursenumber() {
+		return this.coursenumber;
+	}
+	
+	public void setcoursenumber(int coursenumber) {
+		this.coursenumber = coursenumber;
+	}	
+       
+  public int getcredit() {
+     return this.credit = credit;
+  }
+
+  public void setcredit(int credit) {
+     this.credit = credit;
+ }
+
+ public String getprofessor() {
+    return this.professor = professor;
+ }
+
+ public String setprofessor(String professor) {
+     this.professor = professor;
+ }
